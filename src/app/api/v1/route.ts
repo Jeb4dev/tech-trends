069d3deaf3bfe3565111ed98e390b31490b265ab
Jeb <commit@jeb4.dev>
2024-02-24 21:05:58 +0200

    use sqlite3

--- src/app/api/v1/route.ts
+++ src/app/api/v1/route.ts
@@ -1,83 +1,155 @@
-import { ResponseData } from "@/types";
-import { NextResponse } from "next/server";
-import { put, del, list } from "@vercel/blob";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { ResponseData, Results } from "@/types";
 import crypto from "crypto";
+import { NextResponse } from "next/server";
 
-// Variables to store the fetched data and the time of the fetch
-let fetchedData: ResponseData;
-let fetchTime = new Date().getTime();
+let data: Results[] = [];
 
-// Function to create a hash of the data
-function hashData(data: object) {
+function hashData(data: Results) {
+  // Create a hash of the data
+  const item = JSON.stringify({
+    slug: data.slug,
+    heading: data.heading,
+    descr: data.descr,
+    company_name: data.company_name,
+    municipality_name: data.municipality_name,
+  });
+
   const hash = crypto.createHash("sha256");
-  hash.update(JSON.stringify(data));
+  hash.update(JSON.stringify(item));
   return hash.digest("hex");
 }
 
-// Function to fetch data from the API
-async function fetchJobData() {
-  let apiUrl = "https://duunitori.fi/api/v1/jobentries?format=json&search=Tieto-+ja+tietoliikennetekniikka%28ala%29";
+async function jobAlreadyInData(newJob: Results) {
+  // Check if the job already exists in the data, return boolean
+  const hash = hashData(newJob);
+  return !!data.find((job) => hashData(job) === hash);
+}
+
+async function initializeDatabase() {
+  // Connect to SQLite database
+  const db = await open({
+    filename: "./jobs.db",
+    driver: sqlite3.Database,
+  });
+
+  // Create table if it doesn't exist
+  await db.run(`
+    CREATE TABLE IF NOT EXISTS jobs (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      slug TEXT UNIQUE,
+      heading TEXT,
+      descr TEXT,
+      company_name TEXT,
+      municipality_name TEXT
+      date_posted TEXT
+    )
+  `);
+}
+
+async function fetchDataFromApi() {
+  let apiUrl =
+    "https://duunitori.fi/api/v1/jobentries?format=json&page=1&search=Tieto-+ja+tietoliikennetekniikka%28ala%29";
+  let newData: Results[] = [];
+  let duplicates: number = 0;
 
   // Loop to fetch all pages of data from the API
   while (true) {
     console.log(`Fetching ${apiUrl}`);
-    const apiResponse = await fetch(apiUrl);
-    const newPageData: ResponseData = await apiResponse.json();
+    const response = await fetch(apiUrl);
+    const newPageData: ResponseData = await response.json();
 
-    // If the current record already exists in the fetchedData.results array, break the loop
-    if (fetchedData && fetchedData.results.some((result) => hashData(result) === hashData(newPageData.results[0]))) {
-      break;
+    // Loop through each job and add new jobs to the new data
+    for (const job of newPageData.results) {
+      if (await jobAlreadyInData(job)) {
+        duplicates++;
+      } else {
+        newData.push(job);
+      }
     }
+
+    // If there are no more pages, break the loop
     if (!newPageData.next) break;
-    fetchedData =
-      fetchedData != null
-        ? { ...fetchedData, results: fetchedData.results.concat(newPageData.results), next: newPageData.next }
-        : newPageData;
+    // If there are more than 10 duplicates, break the loop
+    if (duplicates > 10) break;
+    // Set the next API URL to fetch
     apiUrl = newPageData.next;
   }
 
-  // Store the time of the fetch
-  fetchTime = new Date().getTime();
+  console.log(`Fetched ${newData.length} new jobs, ${duplicates} duplicates`);
 
-  // Store the fetched data in the blob storage
-  const jobs = await put("jobs/data.json", JSON.stringify(fetchedData), { access: "public" });
-  console.log(jobs);
+  return newData;
 }
 
-// Serverless function to handle GET requests
+async function insertNewJobData(data: Results[]) {
+  const db = await open({
+    filename: "./jobs.db",
+    driver: sqlite3.Database,
+  });
+
+  for (const job of data) {
+    try {
+      await db.run(
+        "INSERT INTO jobs (slug, heading, descr, company_name, municipality_name, date_posted) VALUES (?, ?, ?, ?, ?, ?)",
+        job.slug,
+        job.heading,
+        job.descr,
+        job.company_name,
+        job.municipality_name,
+        job.date_posted
+      );
+    } catch (e) {
+      console.log("Error inserting job data", e);
+    }
+  }
+}
+
+async function fetchDatabaseData() {
+  try {
+    const db = await open({
+      filename: "./jobs.db",
+      driver: sqlite3.Database,
+    });
+    // Fetch existing job data from the database
+    return await db.all("SELECT * FROM jobs");
+  } catch (e) {
+    console.log("Error opening database", e);
+    await initializeDatabase();
+    const db = await open({
+      filename: "./jobs.db",
+      driver: sqlite3.Database,
+    });
+    // Fetch existing job data from the database
+    return await db.all("SELECT * FROM jobs");
+  }
+}
+
 export async function GET() {
-  const blobsData = await list();
-  const newestData = blobsData.blobs.find(
-    (blob) => blob.uploadedAt === blobsData.blobs.reduce((a, b) => (a.uploadedAt > b.uploadedAt ? a : b)).uploadedAt
-  );
-  let fetchTime = new Date().getTime();
+  data = [];
+  // Fetch existing job data from the database
+  const existingJobData = await fetchDatabaseData();
 
-  // delete old data
-  // for (const blob of blobsData.blobs) {
-  //   if (new Date().getTime() - blob.uploadedAt.getTime() > 400000) {
-  //     console.log(`Deleting old data: ${blob.url}`);
-  //     await del(blob.url);
-  //   }
-  // }
+  // Concatenate the existing job data with the data
+  data = existingJobData.concat(data);
 
-  if (newestData) {
-    console.log("Fetching data from the blob storage");
-    fetchedData = JSON.parse(await fetch(newestData.url).then((res) => res.text()));
-    fetchTime = newestData.uploadedAt.getTime();
-    // If newest data is older than 5 minutes, fetch new data
-    if (new Date().getTime() - fetchTime > 300000) {
-      console.log("Fetching new data");
-      await fetchJobData();
-    }
-    else {
-      console.log("Using cached data");
-    }
-  }
-  else {
-    console.log("Fetching new data");
-    await fetchJobData();
-  }
+  // Fetch new job data from the API
+  const newJobData = await fetchDataFromApi();
 
-  // Return the fetched data
-  return NextResponse.json({ data: fetchedData });
+  // Add new job data to the db
+  await insertNewJobData(newJobData);
+
+  // Concatenate the new job data with the data
+  data = newJobData.concat(data);
+
+  // Return the job data
+  const returnData: ResponseData = {
+    count: data.length,
+    next: null,
+    previous: null,
+    results: data,
+  };
+
+  console.log(`Returning ${returnData.count} jobs`);
+  return NextResponse.json({ data: returnData });
 }
