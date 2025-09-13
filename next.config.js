e929314836dca7dd5cd2ef8a6a91f7028ffa8854
jeb4 <commit@jeb4.dev>
2025-09-13 16:17:14 +0300

    add redirect from root to /trends in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/trends',
+        permanent: true,
+      },
+    ]
+  }
 };
 
 module.exports = nextConfig
