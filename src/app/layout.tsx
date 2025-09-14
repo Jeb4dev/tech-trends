28f6f827a30fe772919bcc7068a214d1222c9f63
jeb4 <commit@jeb4.dev>
2025-09-14 18:49:36 +0300

    +analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script defer data-domain="koodaripula.fi" src="http://analytics.bittive.com/js/script.js"></script>
+      </head>
       <body className={`${inter.className} bg-slate-800 container mx-auto p-4`}>{children}</body>
     </html>
   );
